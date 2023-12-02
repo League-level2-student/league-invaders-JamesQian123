5f56442af2f33d56a992284515ebbf5cddf31abb
league <league@imac-29.attlocal.net>
2023-12-01 18:26:50 -0800

    asdf do change the state

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel object;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	LeagueInvaders(){
+		frame = new JFrame();
+		object = new GamePanel();
+		
+	}
+	void setup() {
+		frame.add(object);
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
